731c10ab1965b8461606f9cc0a6e097baf48e8fa
Andre Lucas <alucas748@gmail.com>
2023-02-17 18:20:01 -0300

    Aplicativo finalizado

--- lib/play_page.dart
+++ lib/play_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cara_ou_coroa/result_page.dart';
 import 'package:flutter/material.dart';
 
@@ -11,10 +13,14 @@ class Jogar extends StatefulWidget {
 class _JogarState extends State<Jogar> {
 
   void _exibirResultado (){
+    var itens = ["cara", "coroa"];
+    var numero = Random().nextInt(itens.length);
+    var resultado = itens[numero];
+
     Navigator.push(
       context, 
       MaterialPageRoute(
-        builder: (context) => Resultado()
+        builder: (context) => Resultado(resultado)
       )
     );
   }
--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatefulWidget {
-  const Resultado({super.key});
+  const Resultado(this.valor, {super.key});
+
+  final String valor;
 
   @override
   State<Resultado> createState() => _ResultadoState();
@@ -10,13 +12,22 @@ class Resultado extends StatefulWidget {
 class _ResultadoState extends State<Resultado> {
   @override
   Widget build(BuildContext context) {
+
+    var caminhoImagem = "";
+
+    if(widget.valor == "cara"){
+      caminhoImagem = "assets/images/moeda_cara.png";
+    }else {
+      caminhoImagem = "assets/images/moeda_coroa.png";
+    }
+
     return Scaffold(
       backgroundColor: const Color(0xff61bd86),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Image.asset("assets/images/moeda_cara.png"),
+          Image.asset(caminhoImagem),
           GestureDetector(
             onTap: (){
               Navigator.pop(context);
